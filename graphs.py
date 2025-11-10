4be0a205782d07889d590429423dcb17e8788c50
Michael Franzonello <mfranzonello@gmail.com>
2025-11-10 16:21:47 -0500

    changed streamlit title

--- graphs.py
+++ graphs.py
@@ -5,7 +5,7 @@ from family_tree.statistics import get_engine, fetch_folders
 
 engine = get_engine()
 
-st.title('Hello World')
+st.title('Franzonello Family 2025 YIR WIP')
 
 folder_values = fetch_folders(engine, 2025)
 diplay_values = folder_values[['full_name', 'video_count']].set_index('full_name')
