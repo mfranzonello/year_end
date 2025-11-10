73e1eb263411b5b41fa7127a2c46320426151681
Michael Franzonello <mfranzonello@gmail.com>
2025-11-10 18:03:16 -0500

    for real this time -- removed token functions

--- common/secret.py
+++ common/secret.py
@@ -4,8 +4,6 @@ from os import getenv
 
 from dotenv import dotenv_values, find_dotenv, load_dotenv, set_key
 
-from common.structure import read_json, write_json
-
 dotenv_file = load_dotenv(find_dotenv())
 
 def get_secret(name, default=None):
