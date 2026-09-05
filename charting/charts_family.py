537c64bb00901ca4be6725e6d3651e9ad38105df
Michael Franzonello <mfranzonello@gmail.com>
2026-09-05 09:02:02 -0400

    added staggered subgenerations when generation size is too big

--- charting/charts_family.py
+++ charting/charts_family.py
@@ -1,9 +1,8 @@
 from math import ceil
-from itertools import pairwise
 from uuid import UUID
 
 from graphviz import Graph
-from pandas import DataFrame
+from pandas import DataFrame, isna
 from pandas.api.types import is_list_like
 from webcolors import name_to_hex
 
@@ -61,6 +60,71 @@ def get_attributes(node, use_images=False) -> str:
 
     return attributes
 
+def find_boundaries(tree_data:DataFrame) -> DataFrame:
+    """Assign contiguous family blocks using overlapping visible parent sets."""
+    tree_data['family_unit'] = -1
+    visible_ids = set(tree_data['node_id'])
+    family_unit = -1
+
+    ordered = tree_data.sort_values(['generation', 'x_order'])
+    for _, generation in ordered.groupby('generation', sort=False):
+        tracked_parents:set[UUID] = set()
+
+        for _, unit in generation.groupby('unit_order', sort=False):
+            current_parents:set[UUID] = set()
+            for value in unit['parent_ids']:
+                current_parents.update(_visible_parent_ids(value, visible_ids))
+
+            if family_unit < 0 or (
+                    current_parents
+                    and tracked_parents.isdisjoint(current_parents)):
+                family_unit += 1
+                tracked_parents = set(current_parents)
+            else:
+                tracked_parents.update(current_parents)
+
+            tree_data.loc[unit.index, 'family_unit'] = family_unit
+
+    return tree_data
+
+
+def _visible_parent_ids(value:object, visible_ids:set[UUID]) -> set[UUID]:
+    """Normalize a PostgreSQL UUID array and omit undiscovered parents."""
+    if value is None:
+        return set()
+    values = value if is_list_like(value) and not isinstance(value, str) else [value]
+    return {parent_id for parent_id in values
+            if not isna(parent_id) and parent_id in visible_ids}
+
+def find_display_generations(tree_data:DataFrame, generation_limit:int) -> DataFrame:
+    """Stagger complete families and rebuild cross-family ordering tails."""
+    if generation_limit < 1:
+        raise ValueError('generation_limit must be at least 1.')
+
+    tree_data['display_generation'] = tree_data['generation'].astype(float)
+
+    for generation_value in tree_data['generation'].drop_duplicates():
+        generation = tree_data[(tree_data['generation'] == generation_value)
+                               & (tree_data['node_type'] != 'junction')]
+        family_ids = generation.sort_values('x_order')['family_unit'].drop_duplicates().tolist()
+        display_rows = max(1, ceil(len(generation) / generation_limit))
+        family_rows = {
+            family_id: position % display_rows
+            for position, family_id in enumerate(family_ids)
+        }
+
+        for family_id, display_row in family_rows.items():
+            mask = ((tree_data['generation'] == generation_value)
+                    & (tree_data['family_unit'] == family_id))
+            tree_data.loc[mask, 'display_generation'] = (
+                float(generation_value) + (display_row / display_rows)
+            )
+
+    ##tree_data.loc[tree_data['node_type'] == 'animal', 'display_generation'] += 1/(2*display_rows)
+    tree_data['edge_constraint'] = True #tree_data['display_generation'] == tree_data['display_generation'].shift(-1)
+
+    return tree_data
+
 ''' main family tree charts '''
 def tree_chart(tree_data:DataFrame, cloud_name:str, use_images=False) -> Graph:
     GENERATION_LIMIT = 20
@@ -68,41 +132,42 @@ def tree_chart(tree_data:DataFrame, cloud_name:str, use_images=False) -> Graph:
     tree = Graph()
     ##tree.attr(splines='False')
     ##tree.attr(splines='polyline')
+    ##tree.attr(splines='ortho')
 
-    for generation in tree_data['generation'].unique():
-        tree_data_g = tree_data[tree_data['generation']==generation]
-        generation_size = len(tree_data_g)
-        gx = ceil(generation_size / GENERATION_LIMIT)
+    tree_data = find_boundaries(tree_data)
+    tree_data = find_display_generations(tree_data, GENERATION_LIMIT)
 
-        for i in range(gx + 1):
-            subtree = Graph()
-            subtree.attr(rank='same')
+    for generation in tree_data['display_generation'].unique():
+        tree_data_g = tree_data[tree_data['display_generation']==generation]
 
-            for _, node in tree_data_g.iterrows():
-                node_type = node['node_type']
-                if node_type in ['person', 'animal']:
-                    if use_images:
-                        image = get_image_path(cloud_name, str(node['node_id']))
-                        label = ''
-                    else:
-                        image = None
-                        if node_type == 'person':
-                            label = get_person_label(node)
-                        else:
-                            label = get_animal_label(node)
+        subtree = Graph()
+        subtree.attr(rank='same')
 
+        for _, node in tree_data_g.iterrows():
+            node_type = node['node_type']
+            if node_type in ['person', 'animal']:
+                if use_images:
+                    image = get_image_path(cloud_name, str(node['node_id']))
+                    label = ''
                 else:
-                    if use_images:
-                        label = ''
-                    else:
-                        label = node['clan_name']
                     image = None
+                    if node_type == 'person':
+                        label = get_person_label(node)
+                    else:
+                        label = get_animal_label(node)
 
-                attributes = get_attributes(node, use_images)
+            else:
+                if use_images:
+                    label = ''
+                else:
+                    label = node['clan_name']
+                image = None
 
-                subtree.node(str(node['node_id']), label=label, image=image, tooltip=label, **attributes)
+            attributes = get_attributes(node, use_images)
 
-            tree.subgraph(subtree)
+            subtree.node(str(node['node_id']), label=label, image=image, tooltip=label, **attributes)
+
+        tree.subgraph(subtree)
 
     # add horizontal edges
     for _, node in tree_data[tree_data['tail_id'].notna()].iterrows():
@@ -113,7 +178,10 @@ def tree_chart(tree_data:DataFrame, cloud_name:str, use_images=False) -> Graph:
                 style = None
             case _:
                 style = 'invis'
-        tree.edge(str(node['node_id']), str(node['tail_id']), style=style)
+        tree.edge(str(node['node_id']), str(node['tail_id']),
+                  #constraint='True',
+                  constraint=str(node['edge_constraint']),
+                  style=style)
 
     # add vertical edges
     for _, node in tree_data[tree_data['head_id'].notna()].iterrows():       
